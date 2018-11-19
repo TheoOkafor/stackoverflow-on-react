f5cab75786ed5b9dc49c5eb4537b50e1639d5f11
Theo Okafor <theo.okafor@yahoo.com>
2018-11-19 12:30:51 +0100

    chore(setup): Setup webpack and react environment
    - Install webpack and react
    - Create Index.js and App.js files
    
    [Finishes #161438816]

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+// import path from 'path';
+// import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'index_bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+            loader: 'babel-loader',
+        }
+      }
+    ]
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: './src/index.html',
+    })
+  ],
+}
